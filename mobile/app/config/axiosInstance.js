a9b2669e477de9fa7ec6dfe0254e38eb5fbd7add
angad mane <angadmane007@gmail.com>
2025-06-21 15:56:43 +0530

    git stash

--- mobile/app/config/axiosInstance.js
+++ mobile/app/config/axiosInstance.js
@@ -3,13 +3,10 @@ import axios from 'axios';
 
 //const BASE_URL = 'http://192.168.43.95:3000/api/';
 // const BASE_URL = 'https://jsonplaceholder.typicode.com';
-<<<<<<< HEAD
  const BASE_URL = 'http://192.168.1.123:3000/api';
-=======
 //const BASE_URL = 'http://192.168.1.123:3000/api';
->>>>>>> 016e337d729df3d997e7f799c6d5da58cee28375
 //const BASE_URL = 'https://lsbackend.laxmipanditservices.com/api';
-const BASE_URL = 'http://192.168.1.121:3000/api';
+//const BASE_URL = 'http://192.168.1.121:3000/api';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
