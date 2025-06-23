7e777c8fcb50072db2cf14dc75bf1371287482b5
angad mane <angadmane007@gmail.com>
2025-06-23 11:32:52 +0530

    fix: update BASE_URL configuration for axios instance

--- mobile/app/config/axiosInstance.js
+++ mobile/app/config/axiosInstance.js
@@ -1,9 +1,9 @@
 import { View, Text, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.43.95:3000/api/';
+//const BASE_URL = 'http://192.168.43.95:3000/api/';
 // const BASE_URL = 'https://jsonplaceholder.typicode.com';
-// const BASE_URL = 'http://192.168.1.123:3000/api';
+ const BASE_URL = 'http://192.168.1.123:3000/api';
 //const BASE_URL = 'http://192.168.1.123:3000/api';
 //const BASE_URL = 'https://lsbackend.laxmipanditservices.com/api';
 //const BASE_URL = 'http://192.168.1.121:3000/api';
